60c1d7d60d7218d9ff591f65af21fbe622fffb9e
ADMIN <tnthanh565@gmail.com>
2023-10-24 07:33:19 +0700

    trangql

---
+++ app/src/main/java/com/example/appdocbao/admin.java
@@ -0,0 +1,31 @@
+package com.example.appdocbao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class admin extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        recyclerView = findViewById(R.id.rcvAdmin);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin.this, Add.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
