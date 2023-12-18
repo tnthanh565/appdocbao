673253230d7cdc1a260ae5bb67dadd63473b2c25
THIS PC <quanglinh0422@gmail.com>
2023-12-18 20:09:03 +0700

    aa

--- app/src/main/java/com/example/appdocbao/kp.java
+++ app/src/main/java/com/example/appdocbao/kp.java
@@ -3,6 +3,7 @@ package com.example.appdocbao;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,20 +14,12 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-import demo.bxh;
-import demo.bxhAdapter;
-import demo.hot;
-import demo.hotAdapter;
+import demo.NewsAdapter;
+import demo.kp2Adapter;
+import demo.news;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link kp#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class kp extends Fragment {
-    private List<bxh> bxhList;
-    RecyclerView rcvBXH;
-    private demo.bxhAdapter bxhAdapter;
 
     public kp() {
         // Required empty public constructor
@@ -37,26 +30,18 @@ public class kp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_kp, container, false);
+        View view = inflater.inflate(R.layout.fragment_kp, container, false);
+        database db = new database(getActivity());
+        ArrayList<news> news = db.getAllnews();
+        RecyclerView rcv = view.findViewById(R.id.rcvKp2);
+        kp2Adapter adapter = new kp2Adapter(getActivity(), news);
+        rcv.setAdapter(adapter);
+        rcv.hasFixedSize();
+        rcv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+        rcv.addItemDecoration(itemDecoration);
 
-        bxhList = addList();
-
-        rcvBXH = rootView.findViewById(R.id.rcvBXH);
-        rcvBXH.setLayoutManager(new LinearLayoutManager(getActivity()));
-        bxhAdapter = new bxhAdapter(bxhList);
-        rcvBXH.setAdapter(bxhAdapter);
-        return rootView;
+        return view;
     }
-    private List<bxh> addList(){
-        List<bxh> bxhs = new ArrayList<>();
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "jgfhksf"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "khglsdaja"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "jbkjs"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "người chồng bất lực"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "anh thợ sưả ống nước"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "QUản lý"));
-        bxhs.add(new bxh(R.drawable.ic_launcher_background, "Con ngoan"));
 
-        return bxhs;
-    }
 }
